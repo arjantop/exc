3a7c466f866a405ff277eab8351f9d10ae8e6439
Arjan Topolovec <arjan@celtra.com>
2018-03-12 20:35:29 +0100

    Remove echo parameter.

--- db/__init__.py
+++ db/__init__.py
@@ -1,3 +1,3 @@
 from sqlalchemy import create_engine
 
-Engine = create_engine('mysql://root:test@db:3306/exchange?use_unicode=1', encoding='utf8', echo=True)
+Engine = create_engine('mysql://root:test@db:3306/exchange?use_unicode=1', encoding='utf8')
